a4c8c5caae10994ee207b7451c3131892154d5e9
Wilson Burgos <wilson.burgos@ngc.com>
2016-11-29 20:39:13 -0500

    Added new functionality and javadocs

--- RSA/src/main/java/cs/fit/edu/RSAPrivateKey.java
+++ RSA/src/main/java/cs/fit/edu/RSAPrivateKey.java
@@ -3,10 +3,343 @@
  */
 package cs.fit.edu;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.Random;
+
 /**
  * @author J28243
  *
  */
 public class RSAPrivateKey extends RSAKey {
 
+	private BigInteger privateExponent;
+	private BigInteger p;
+	private BigInteger q;
+	BigInteger dP;
+	BigInteger dQ;
+	BigInteger qInv;
+	
+	/**
+	 * Returns the private exponent value
+	 * @return the private exponent value
+	 */
+	public BigInteger getPrivateExponent() {
+		return privateExponent;
+	}
+
+	/**
+	 * Sets the private exponent value 
+	 * @param privateExponent the private exponent
+	 */
+	public void setPrivateExponent(BigInteger privateExponent) {
+		this.privateExponent = privateExponent;
+		if( p != null ) {
+			//recalculate dp
+			setP(p);
+		}
+		if( q != null ) {
+			//recalculate dq
+			setQ(q);
+		}
+	}
+
+	/**
+	 * Returns the prime P value
+	 * @return the prime p Value
+	 */
+	public BigInteger getP() {
+		return p;
+	}
+
+	/**
+	 * Sets the prime p value
+	 * @param p the prime value
+	 */
+	public void setP(BigInteger p) {
+		if( p != null ) {
+			this.p = p;
+			if( privateExponent != null ) {
+				setdP(privateExponent.mod(p.subtract(BigInteger.ONE)));
+			}
+			//update qInv if q != null
+			if( q != null ) {
+				setqInv(q.modInverse(p));
+			}
+	//		BigInteger qInv = q.modInverse(p);
+		}
+	}
+
+	/**
+	 * Returns the fast decryption CRT dP value
+	 * @return the current value
+	 */
+	public BigInteger getdP() {
+		return dP;
+	}
+
+	/**
+	 * Sets the fast decryption CRT dP value
+	 * @param dP the dp value
+	 */
+	public void setdP(BigInteger dP) {
+		this.dP = dP;
+	}
+
+	/**
+	 * Returns the fast decryption CRT dQ value
+	 * @return the current value
+	 */
+	public BigInteger getdQ() {
+		return dQ;
+	}
+
+	/**
+	 * Sets the fast decryption CRT dQ value
+	 * @param dQ the value
+	 */
+	public void setdQ(BigInteger dQ) {
+		if( dQ != null ) {
+			this.dQ = dQ;
+		}
+	}
+
+	/**
+	 * Returns the q inverse, c coefficient
+	 * @return the q inverse value
+	 */
+	public BigInteger getqInv() {
+		return qInv;
+	}
+
+	/**
+	 * Stores the q inverse, c coefficient
+	 * @param qInv the value to use
+	 */
+	public void setqInv(BigInteger qInv) {
+		this.qInv = qInv;
+	}
+
+	/**
+	 * Rerturns the value of the prime Q
+	 * @return the q Value
+	 */
+	public BigInteger getQ() {
+		return q;
+	}
+
+	/**
+	 * Sets the value of the prime Q
+	 * @param q prime value
+	 */
+	public void setQ(BigInteger q) {
+		if( q != null ) {
+			this.q = q;
+			if( privateExponent != null ) {
+				setdQ(privateExponent.mod(q.subtract(BigInteger.ONE)));
+			}
+			
+			if( p != null ) {
+				setqInv(q.modInverse(p));
+			}
+		}
+	}
+
+
+	/**
+	 * Generate Key using Rabin Miller test certainty and bitlength
+	 * @param bitLength the bitlength to use
+	 * @param certainty the Rabin Miller certainty
+	 * @return RSAPrivate keys
+	 */
+	public static RSAPrivateKey generateKey(int bitLength,double certainty) {
+		// create a random object
+		Random rnd = new Random();
+		
+		// assign probablePrime result to bi using bitLength and rnd
+		// static method is called using class name
+		BigInteger p = BigInteger.probablePrime(bitLength, rnd);
+		// p = new BigInteger("17");
+		int c = (int) (Math.log(1 - certainty) / Math.log(0.5));
+		if (p.isProbablePrime(c)) {
+			String str = "ProbablePrime of bitlength " + bitLength + " is p=" + p;
+			// print bi value
+		//					System.out.println(str);
+		}
+		
+		BigInteger q;
+		
+		do {
+			q = BigInteger.probablePrime(bitLength, new Random());
+		} while (q.equals(p));
+		
+		// q = new BigInteger("11");
+		if (q.isProbablePrime(c)) {
+			String str = "ProbablePrime of bitlength " + bitLength + " is q=" + q;
+			// print bi value
+		//					System.out.println(str);
+		}
+		
+		BigInteger n = q.multiply(p);
+		
+		// compute euler totient
+		// euler theorem & prime factorization
+		// assumption q & p primer
+		BigInteger phi = totient(p, q);
+		
+		// select e random
+		BigInteger e;
+		// make sure gcd(phi,e)==1
+		do {
+			e = BigInteger.probablePrime(phi.bitLength() - 1, new Random());
+		} while (!e.gcd(phi).equals(BigInteger.ONE));
+		
+		// store d
+		BigInteger d = e.modInverse(phi);
+			
+		RSAPrivateKey pp = new RSAPrivateKey();
+		pp.setExponent(e);
+		pp.setModulus(n);
+		pp.setP(p);
+		pp.setQ(q);
+		pp.setPrivateExponent(d);
+		
+		return pp;
+	}
+ 	/**
+	 * Generates a private & public key pair
+	 * @param privFilename the filename for the private key
+	 * @param pubFileName the filename for the public key
+	 */
+	public void saveKeyPair(String privFilename,String pubFileName) {
+		savePrivateKey(privFilename);
+		savePublic(pubFileName);
+	}
+	
+	/**
+	 * 
+	 * @param privFilename
+	 * @param pubFileName
+	 */
+	public void savePrivateKey(String privFilename) {
+		Path path = Paths.get(privFilename);
+		String msg = marshall();
+		
+		try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+			writer.write(msg);
+			msg = msg.replace(',', '\n').replace("(", "(\n");
+			System.out.println(msg);
+		} catch (IOException e3) {
+			// TODO Auto-generated catch block
+			e3.printStackTrace();
+		}
+
+	}
+	@Override
+	public void unmarshall(String obj) {
+		//check if string exist
+		if( obj != null && !obj.isEmpty()) {
+			String[] tokens = obj.trim().split("[\\(\\,\\)\\:]");
+			if( tokens.length == 17) {
+				BigInteger m = new BigInteger(tokens[2].trim(),16);
+				setModulus(m);
+				BigInteger e = new BigInteger(tokens[4].trim(),16);
+				setExponent(e);
+				BigInteger pe = new BigInteger(tokens[6].trim(),16);
+				setPrivateExponent(pe);
+				BigInteger p = new BigInteger(tokens[8].trim(),16);
+				setP(p);
+				BigInteger q = new BigInteger(tokens[10].trim(),16);
+				setQ(q);
+				BigInteger dp = new BigInteger(tokens[12].trim(),16);
+				setdP(dp);
+				BigInteger dq = new BigInteger(tokens[14].trim(),16);
+				setdQ(dq);
+				BigInteger qinv = new BigInteger(tokens[16].trim(),16);
+				setqInv(qinv);
+			}
+		}
+	}
+
+	// RSAPrivateKey: private ( modulus: n, publicExponent: e, privateExponent:
+	// d, prime1: p, prime2: q, exponent1: d mod (p-1), exponent2: d mod (q-1),
+	// coefficient: q-1 mod p)
+
+	@Override
+	public String marshall() {
+		String nHex = modulus.toString(16);
+		String eHex = exponent.toString(16);
+		String dHex = privateExponent.toString(16);
+		String pHex = p.toString(16);
+		String qHex = q.toString(16);
+		String dpHex = dP.toString(16);
+		String dqHex = dQ.toString(16);
+		String qInvHex = qInv.toString(16);
+		StringBuilder str = new StringBuilder();
+		
+		str.append("private ( modulus: ");
+		str.append(nHex);
+		str.append(", publicExponent: ");
+		str.append(eHex);
+		str.append(", privateExponent: ");
+		str.append(dHex);
+		str.append(", prime1: ");
+		str.append(pHex);
+		str.append(", prime2: ");
+		str.append(qHex);
+		str.append(", exponent1: ");
+		str.append(dpHex);
+		str.append(", exponent2: ");
+		str.append(dqHex);
+		str.append(", coefficient: ");
+		str.append(qInvHex);
+		
+		
+		str.append(")");
+		
+		return str.toString();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+	    // self check
+	    if (this == o)
+	        return true;
+	    // null check
+	    if (o == null)
+	        return false;
+	    // type check and cast
+	    if (getClass() != o.getClass())
+	        return false;
+	    RSAPrivateKey key = (RSAPrivateKey) o;
+	    // field comparison
+	    return super.equals((RSAKey)o)
+	            && Objects.equals(privateExponent, key.privateExponent)
+	            && Objects.equals(p,key.p) && Objects.equals(q,key.q)
+	            && Objects.equals(dP, key.dP) && Objects.equals(dQ, key.dQ)
+	            && Objects.equals(qInv, key.qInv);
+	} 
+	
+	/**
+	 * Generate the Euler totient for p & q
+	 * @param p the p prime to use
+	 * @param q the q prime to use
+	 * @return the Euler totient
+	 */
+	public static BigInteger totient(BigInteger p, BigInteger q) {
+		// make sure GCD = 1
+		BigInteger t = p.gcd(q);
+		if (!t.equals(BigInteger.ONE)) {
+			throw new IllegalArgumentException("p & q GCD != 1");
+		}
+		BigInteger a = p.subtract(BigInteger.ONE);
+		BigInteger b = q.subtract(BigInteger.ONE);
+
+		return a.multiply(b);
+	}
 }
--- RSA/src/main/java/cs/fit/edu/RabinMiller.java
+++ RSA/src/main/java/cs/fit/edu/RabinMiller.java
@@ -3,14 +3,8 @@
  */
 package cs.fit.edu;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.math.BigInteger;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Random;
 
 /**
@@ -27,25 +21,8 @@ public class RabinMiller {
 	private static final String SECKEY_FILENAME_DEFAULT = "secret.key";
 	private static final String CERTAINTY_KEY = "-y";
 	private static final String CERTAINTY_DEFAULT = "0.99999";
-	private String publicKeyFilename;
+
 	
-	public String getPublicKeyFilename() {
-		return publicKeyFilename;
-	}
-
-	public void setPublicKeyFilename(String publicKeyFilename) {
-		this.publicKeyFilename = publicKeyFilename;
-	}
-
-	private String privateKeyFilename;
-
-	public String getPrivateKeyFilename() {
-		return privateKeyFilename;
-	}
-
-	public void setPrivateKeyFilename(String privateKeyFilename) {
-		this.privateKeyFilename = privateKeyFilename;
-	}
 
 	/**
 	 * Test based on Fermat's Theorem
@@ -105,61 +82,16 @@ public class RabinMiller {
 		System.out.println("\n");
 	}
 
-	public static void generatePublic(BigInteger n, BigInteger e,String pubFilename) {
-		String nHex = n.toString(16);
-		String eHex = e.toString(16);
-		Path path = Paths.get(pubFilename);
-		
-		try (BufferedWriter writer = Files.newBufferedWriter(path)) {
-			writer.write("public ( modulus: "+nHex+", publicExponent: "+eHex+")");
-			
-			System.out.println("public (");
-			System.out.println("n="+nHex);
-			System.out.println("e="+eHex+" )");
-		} catch (IOException e3) {
-			// TODO Auto-generated catch block
-			e3.printStackTrace();
-		}
 
-	}
 	// RSAPrivateKey: private ( modulus: n, publicExponent: e, privateExponent:
 	// d, prime1: p, prime2: q, exponent1: d mod (p-1), exponent2: d mod (q-1),
 	// coefficient: q-1 mod p)
 	// RSAPublicKey: public (modulus: n, publicExponent: e)
+////create message by converting string to integer
+    // String s = "test";
+    // byte[] bytes = s.getBytes();
+    // BigInteger message = new BigInteger(bytes);
 
-	public static void generatePrivate(BigInteger n, BigInteger e, BigInteger d, BigInteger p, BigInteger q, BigInteger e1,
-			BigInteger e2, BigInteger c,String privFilename) {
-		Path path = Paths.get(privFilename);
-		
-		String nHex = n.toString(16);
-		
-		String eHex = e.toString(16);
-		String dHex = d.toString(16);
-		String pHex = p.toString(16);
-		String qHex = q.toString(16);
-		String dPHex = e1.toString(16);
-		String dQHex = e2.toString(16);
-		String qInvHex = c.toString(16);
-		
-		try (BufferedWriter writer = Files.newBufferedWriter(path)) {
-			writer.write("private ( modulus: "+nHex+", publicExponent: "+eHex+", privateExponent: "+dHex+", prime1: "+pHex+", prime2: "+qHex);
-			writer.write(", exponent1: "+dPHex+", exponent2: "+dQHex+", coefficient: "+qInvHex+")");
-			
-			System.out.println("private (");
-			System.out.println("n="+nHex);
-			System.out.println("e="+eHex);
-			System.out.println("d="+dHex);
-			System.out.println("p="+pHex);
-			System.out.println("q="+qHex);
-			System.out.println("dP="+dPHex);
-			System.out.println("dQ="+dQHex);
-			System.out.println("qInv="+qInvHex+ ")");
-		} catch (IOException e3) {
-			// TODO Auto-generated catch block
-			e3.printStackTrace();
-		}
-
-	}
 
 	public static void main(String[] args) {
 
@@ -195,85 +127,29 @@ public class RabinMiller {
 		String publicKeyFileName = argMap.getOrDefault(PUBKEY_FILENAME_KEY, PUBKEY_FILENAME_DEFAULT);
 		String secretKeyFileName = argMap.getOrDefault(SECKEY_FILENAME_KEY, SECKEY_FILENAME_DEFAULT);
 
-		// create a random object
-		Random rnd = new Random();
-
-		// assign probablePrime result to bi using bitLength and rnd
-		// static method is called using class name
-		p = BigInteger.probablePrime(bitLength, rnd);
-		// p = new BigInteger("17");
-		int c = (int) (Math.log(1 - certainty) / Math.log(0.5));
-		if (p.isProbablePrime(c)) {
-			String str = "ProbablePrime of bitlength " + bitLength + " is p=" + p;
-			// print bi value
-//			System.out.println(str);
-		}
-
-		BigInteger q;
-
-		do {
-			q = BigInteger.probablePrime(bitLength, new Random());
-		} while (q.equals(p));
-
-		// q = new BigInteger("11");
-		if (q.isProbablePrime(c)) {
-			String str = "ProbablePrime of bitlength " + bitLength + " is q=" + q;
-			// print bi value
-//			System.out.println(str);
-		}
-
-		BigInteger n = q.multiply(p);
-
-		// compute euler totient
-		// euler theorem & prime factorization
-		// assumption q & p primer
-		BigInteger phi = totient(p, q);
-
-		// select e random
-		BigInteger e;
-		// make sure gcd(phi,e)==1
-		do {
-			e = BigInteger.probablePrime(phi.bitLength() - 1, new Random());
-		} while (!e.gcd(phi).equals(BigInteger.ONE));
-
-		// store d
-		BigInteger d = e.modInverse(phi);
+		//generate key
+		RSAPrivateKey pp = RSAPrivateKey.generateKey(bitLength, certainty);
 		
-		//CRT
-		BigInteger dP = d.mod(p.subtract(BigInteger.ONE));
-		BigInteger dQ = d.mod(q.subtract(BigInteger.ONE));
-		BigInteger qInv = q.modInverse(p);
-//		System.out.println("Public Key: e=" + e + " n=" + n);
-//		System.out.println("Private Key: d=" + d + " p=" + p + " q=" + q);
-		
-		generatePrivate( n,e,d,p,q,dP,dQ,qInv,secretKeyFileName);
-		generatePublic( n,e,publicKeyFileName);
+		pp.saveKeyPair(secretKeyFileName,publicKeyFileName);
 		// double cert = 1 - Math.pow(4, -10);
 		// for (int i = 0; i < 10; i++) {
 		// System.out.println(isPrime(p));
 		// }
 		RSAKey rr = new RSAKey();
 		
-		rr.setExponent(e);
-		rr.setModulus(n);
+		rr.setExponent(pp.getExponent());
+		rr.setModulus(pp.getModulus());
 		
 		RSAKey zz = new RSAKey();
 		zz.unmarshall(rr.marshall());
 
-	}
-
-	//
-
-	public static BigInteger totient(BigInteger p, BigInteger q) {
-		// make sure GCD = 1
-		BigInteger t = p.gcd(q);
-		if (!t.equals(BigInteger.ONE)) {
-			throw new IllegalArgumentException("p & q GCD != 1");
-		}
-		BigInteger a = p.subtract(BigInteger.ONE);
-		BigInteger b = q.subtract(BigInteger.ONE);
-
-		return a.multiply(b);
+		System.out.println(zz.equals(rr));
+	
+		
+		RSAPrivateKey yy = new RSAPrivateKey();
+		yy.unmarshall(pp.marshall());
+		
+		System.out.println(pp.equals(yy)+ " " +pp.equals(new RSAPrivateKey()));	
 	}
 
 }
